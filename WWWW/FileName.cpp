d347f064d7401231e597681c6a2837fe7a6251df
kovaar10 <kovaar10@29-8>
2025-09-05 11:49:46 +0300

    dz 05.09

--- WWWW/FileName.cpp
+++ WWWW/FileName.cpp
@@ -8,17 +8,30 @@ class Point
 private:
 	int x;
 	int y;
+	int s;
 public:
-	Point()
+	void Print()
 	{
+		cout << s << "->X: " << x << "\tY: " << y << endl;
+	}
+	void Init(char t, int _x, int _y)
+	{
+		s = t;
+		x = _x;
+		y = _y;
+	}
+	void Sum(Point b)
+	{
+		cout << "X: " << x + b.x << "\tY: " << y + b.y << endl;
+	}
 
+	void Min(Point b)
+	{
+		cout << "X: " << x - b.x << "\tY: " << y - b.y << endl;
+	}
+
+	void Mult(Point b)
+	{
+		cout << "X: " << x * b.x << "t\Y: " << y * b.y << endl;
 	}
 };
-
-
-
-
-int main()
-{
-	cout << "Hello world!!\n";
-}
